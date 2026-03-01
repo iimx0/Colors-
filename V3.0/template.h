acc857e0a87c9d4330e1e33823a9e96932a5ac01
iimx0 <dastlenet@gmail.com>
2026-03-01 13:43:09 +0500

    Delete V3.0 directory

--- V3.0/template.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEMPLATE_H
-#define TEMPLATE_H
-
-#include <string>
-#include "config_main.h"
-
-std::string apply_template(const UserConfig& config, const std::string& username, const std::string& current_dir, const std::string& symbol);
-
-#endif
